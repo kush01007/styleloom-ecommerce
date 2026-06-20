c4ae3087c9aad6f0c4cb39fde7614503713a9984
Kush Gupta <kushtech123@gmail.com>
2026-06-20 07:10:22 +0530

    plss

--- Backend/server.js
+++ Backend/server.js
@@ -8,23 +8,35 @@ import productRouter from './routes/productRoute.js'
 import cartRouter from './routes/cartRoute.js'
 import orderRouter from './routes/orderRoute.js'
 
-//App config
-const app=express();
-const port= process.env.PORT || 4000
+const app = express()
+const port = process.env.PORT || 4000
+
 connectDB()
 connectCloudinary()
 
-//middlewares
 app.use(express.json())
-app.use(cors());
 
-//api endpoints
-app.use('/api/user',userRouter);
-app.use('/api/product', productRouter);
-app.use('/api/cart', cartRouter);
-app.use('/api/order', orderRouter);
-app.get('/',(req,res)=>{
-    res.send("API working")
+app.use(cors({
+origin: [
+'http://localhost:5173',
+'http://localhost:5174',
+'https://styleloom-ecommerce-proj.vercel.app',
+'https://styleloom-ecommerce.vercel.app',
+'https://styleloom-ecommerce-admin.vercel.app'
+
+
+],
+methods: ['GET', 'POST', 'PUT', 'DELETE'],
+credentials: true
+}))
+
+app.use('/api/user', userRouter)
+app.use('/api/product', productRouter)
+app.use('/api/cart', cartRouter)
+app.use('/api/order', orderRouter)
+
+app.get('/', (req, res) => {
+res.send('API working')
 })
 
-app.listen(port,()=>console.log('server started on port:'+port))
+app.listen(port, () => console.log('server started on port:' + port))
